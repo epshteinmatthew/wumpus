b1126e21c1d3c21c6b5065db422429d0e9adc281
robotmeatsuit <robotmeatsuit@gmail.com>
2023-05-11 13:33:10 -0700

    ASFHDJGK

--- Wumpus/StartMenu.Designer.cs
+++ Wumpus/StartMenu.Designer.cs
@@ -41,7 +41,7 @@
             this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.button2.ForeColor = System.Drawing.Color.White;
-            this.button2.Location = new System.Drawing.Point(319, 193);
+            this.button2.Location = new System.Drawing.Point(319, 220);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(93, 30);
             this.button2.TabIndex = 7;
@@ -55,7 +55,7 @@
             this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.button1.ForeColor = System.Drawing.Color.White;
-            this.button1.Location = new System.Drawing.Point(319, 157);
+            this.button1.Location = new System.Drawing.Point(319, 184);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(93, 30);
             this.button1.TabIndex = 6;
@@ -69,7 +69,7 @@
             this.label1.BackColor = System.Drawing.Color.Black;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label1.ForeColor = System.Drawing.SystemColors.Control;
-            this.label1.Location = new System.Drawing.Point(152, 88);
+            this.label1.Location = new System.Drawing.Point(171, 43);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(389, 55);
             this.label1.TabIndex = 4;
