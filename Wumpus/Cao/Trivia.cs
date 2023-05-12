17f82c96df57bf8b0093b2a5068343a9555298e8
Christopher Cao <2001180@apps.nsd.org>
2023-05-12 12:50:52 -0700

    new form

---
+++ Wumpus/Cao/Trivia.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Wumpus.Cao
+{
+    public partial class Trivia : Form
+    {
+            Random RandomGenerator = new Random();
+            //.OrderBy(_=>generator.Next()).ToArray();
+            Dictionary<string, List<string>> questions = new Dictionary<string, List<string>>()
+        {
+            {"When was the KGB founded?",  new List<string> {"1953", "1954", "1955", "1964" } },
+            {"Who plays Lavrentiy Beria in the movie The Death of Stalin",  new List<string> {"George Beale Russell", "Jeffery Tambor", "Steve Buscemi", "Jason Isaacs" } },
+            {"In what bomber did Putin fly in during an inspection?", new List<string>{"Tu-160M2", "Tu-160M", "Tu-160S", "Tu-160" } },
+            {"What is Putin's shoe size?", new List<string>{"9", "8", "7", "10"} },
+            {"Which KGB directorate did Putin work for?", new List<string>{"2nd", "1st", "3rd", "4th" } },
+            {"Which of these countries did not withdraw its signature from the Rome Statute?", new List<string>{"China", "Russia", "USA", "Sudan" } },
+            {"How many countries are state parties to the Rome Statute?", new List<string>{"123", "139", "112", "90"} },
+            {"What discipline did Putin receive his PhD in?", new List<string>{"Economics", "Finance", "Business", "Mechanical Engineering" } },
+
+        };
+
+        public int suddenTrivia();
+
+           
+            public Trivia()
+        {
+            InitializeComponent();
+        }
+
+        private void Trivia_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
