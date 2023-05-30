e46cbe1a12cdd37ad779736567257fc0a265727b
robotmeatsuit <robotmeatsuit@gmail.com>
2023-05-30 13:15:58 -0700

    edits in leaderboard: times work now! ez claps

--- Wumpus/Epshtein/Leaderboard.Designer.cs
+++ Wumpus/Epshtein/Leaderboard.Designer.cs
@@ -50,59 +50,67 @@ namespace Wumpus.Epshtein
             // leaderboardListBox
             // 
             this.leaderboardListBox.FormattingEnabled = true;
-            this.leaderboardListBox.ItemHeight = 20;
-            this.leaderboardListBox.Location = new System.Drawing.Point(58, 81);
+            this.leaderboardListBox.Location = new System.Drawing.Point(39, 53);
+            this.leaderboardListBox.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.leaderboardListBox.Name = "leaderboardListBox";
-            this.leaderboardListBox.Size = new System.Drawing.Size(191, 304);
+            this.leaderboardListBox.Size = new System.Drawing.Size(129, 199);
             this.leaderboardListBox.TabIndex = 0;
             this.leaderboardListBox.SelectedIndexChanged += new System.EventHandler(this.leaderboardList_SelectedIndexChanged);
             // 
             // label1
             // 
-            this.label1.Location = new System.Drawing.Point(268, 85);
+            this.label1.Location = new System.Drawing.Point(179, 55);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(122, 22);
+            this.label1.Size = new System.Drawing.Size(81, 14);
             this.label1.TabIndex = 1;
             this.label1.Text = "Name";
             // 
             // label2
             // 
-            this.label2.Location = new System.Drawing.Point(268, 129);
+            this.label2.Location = new System.Drawing.Point(179, 84);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(122, 22);
+            this.label2.Size = new System.Drawing.Size(81, 14);
             this.label2.TabIndex = 2;
             this.label2.Text = "Score";
             // 
             // label3
             // 
-            this.label3.Location = new System.Drawing.Point(268, 176);
+            this.label3.Location = new System.Drawing.Point(179, 114);
+            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(122, 22);
+            this.label3.Size = new System.Drawing.Size(81, 14);
             this.label3.TabIndex = 3;
             this.label3.Text = "Time Spent";
             this.label3.Click += new System.EventHandler(this.label3_Click);
             // 
             // textBoxName
             // 
-            this.textBoxName.Location = new System.Drawing.Point(368, 81);
+            this.textBoxName.Enabled = false;
+            this.textBoxName.Location = new System.Drawing.Point(245, 53);
+            this.textBoxName.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.textBoxName.Name = "textBoxName";
             this.textBoxName.ReadOnly = true;
-            this.textBoxName.Size = new System.Drawing.Size(134, 26);
+            this.textBoxName.Size = new System.Drawing.Size(91, 20);
             this.textBoxName.TabIndex = 4;
             // 
             // textBoxScore
             // 
-            this.textBoxScore.Location = new System.Drawing.Point(368, 125);
+            this.textBoxScore.Enabled = false;
+            this.textBoxScore.Location = new System.Drawing.Point(245, 81);
+            this.textBoxScore.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.textBoxScore.Name = "textBoxScore";
             this.textBoxScore.ReadOnly = true;
-            this.textBoxScore.Size = new System.Drawing.Size(134, 26);
+            this.textBoxScore.Size = new System.Drawing.Size(91, 20);
             this.textBoxScore.TabIndex = 5;
             // 
             // radioButtonTime
             // 
-            this.radioButtonTime.Location = new System.Drawing.Point(58, 460);
+            this.radioButtonTime.Location = new System.Drawing.Point(39, 299);
+            this.radioButtonTime.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.radioButtonTime.Name = "radioButtonTime";
-            this.radioButtonTime.Size = new System.Drawing.Size(214, 34);
+            this.radioButtonTime.Size = new System.Drawing.Size(143, 22);
             this.radioButtonTime.TabIndex = 7;
             this.radioButtonTime.TabStop = true;
             this.radioButtonTime.Text = "Time (Lowest to Highest";
@@ -111,9 +119,10 @@ namespace Wumpus.Epshtein
             // 
             // radioButtonScore
             // 
-            this.radioButtonScore.Location = new System.Drawing.Point(58, 421);
+            this.radioButtonScore.Location = new System.Drawing.Point(39, 274);
+            this.radioButtonScore.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.radioButtonScore.Name = "radioButtonScore";
-            this.radioButtonScore.Size = new System.Drawing.Size(237, 33);
+            this.radioButtonScore.Size = new System.Drawing.Size(158, 21);
             this.radioButtonScore.TabIndex = 8;
             this.radioButtonScore.TabStop = true;
             this.radioButtonScore.Text = "Score (Highest to Lowest)";
@@ -122,34 +131,39 @@ namespace Wumpus.Epshtein
             // 
             // label4
             // 
-            this.label4.Location = new System.Drawing.Point(58, 396);
+            this.label4.Location = new System.Drawing.Point(39, 257);
+            this.label4.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(122, 22);
+            this.label4.Size = new System.Drawing.Size(81, 14);
             this.label4.TabIndex = 9;
             this.label4.Text = "Order By";
             // 
             // label5
             // 
-            this.label5.Location = new System.Drawing.Point(58, 60);
+            this.label5.Location = new System.Drawing.Point(39, 39);
+            this.label5.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(214, 22);
+            this.label5.Size = new System.Drawing.Size(143, 14);
             this.label5.TabIndex = 10;
             this.label5.Text = "Hunt the Putin Leaderboard";
             this.label5.Click += new System.EventHandler(this.label5_Click);
             // 
             // textBoxTime
             // 
-            this.textBoxTime.Location = new System.Drawing.Point(368, 172);
+            this.textBoxTime.Enabled = false;
+            this.textBoxTime.Location = new System.Drawing.Point(245, 112);
+            this.textBoxTime.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.textBoxTime.Name = "textBoxTime";
             this.textBoxTime.ReadOnly = true;
-            this.textBoxTime.Size = new System.Drawing.Size(134, 26);
+            this.textBoxTime.Size = new System.Drawing.Size(91, 20);
             this.textBoxTime.TabIndex = 11;
             // 
             // buttonBack
             // 
-            this.buttonBack.Location = new System.Drawing.Point(268, 342);
+            this.buttonBack.Location = new System.Drawing.Point(179, 222);
+            this.buttonBack.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.buttonBack.Name = "buttonBack";
-            this.buttonBack.Size = new System.Drawing.Size(146, 43);
+            this.buttonBack.Size = new System.Drawing.Size(97, 28);
             this.buttonBack.TabIndex = 12;
             this.buttonBack.Text = "Back to Menu";
             this.buttonBack.UseVisualStyleBackColor = true;
@@ -157,9 +171,9 @@ namespace Wumpus.Epshtein
             // 
             // Leaderboard
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(540, 515);
+            this.ClientSize = new System.Drawing.Size(360, 335);
             this.Controls.Add(this.buttonBack);
             this.Controls.Add(this.textBoxTime);
             this.Controls.Add(this.label5);
@@ -172,11 +186,13 @@ namespace Wumpus.Epshtein
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.leaderboardListBox);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "Leaderboard";
             this.Text = "Leaderboard";
             this.Load += new System.EventHandler(this.Leaderboard_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
+
         }
 
         private System.Windows.Forms.Button buttonBack;
